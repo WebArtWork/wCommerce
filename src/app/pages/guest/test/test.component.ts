5c64925f8a8f50834b62cf840f4469bc68e5d84e
nikiquickie <danzi6386@gmail.com>
2024-11-27 14:26:50 +0200

    update code

---
+++ src/app/pages/guest/test/test.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+    templateUrl: './test.component.html',
+    styleUrls: ['./test.component.scss'],
+    standalone: false
+})
+export class TestComponent {}
